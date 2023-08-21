ac3688e8a86c42b14b867b006fb9074340701c6c
MuratArs1an <kilicarslan87@gmail.com>
2023-08-21 23:46:20 +0300

    Spring Boot works- setter injection

---
+++ 02-springCoreDemo/02-setter-injection-qualifiers/src/main/java/com/arslan/springCoreDemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.arslan.springCoreDemo.rest;
+
+import com.arslan.springCoreDemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach;
+    // define a setter for dependency injection
+    @Autowired
+    public void setCoach(@Qualifier("tennisCoach") Coach coach){
+        myCoach=coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
